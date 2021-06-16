0a5d4d9e23099ba057f5789092ee5b30c898eaff
Mansi Wakharkar <mansiswakharkar@gmail.com>
2021-06-16 23:49:03 +0300

    Mixed variable declaration

---
+++ Go/Basics/MixedVariableDeclaration.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var x, y, z = 1, 2.34, -20
+
+	fmt.Println(x)
+	fmt.Printf("x is of type %T\n\n", x)
+	fmt.Println(y)
+	fmt.Printf("y is of type %T\n\n", y)
+	fmt.Println(z)
+	fmt.Printf("z is of type %T\n\n", z)
+
+}
